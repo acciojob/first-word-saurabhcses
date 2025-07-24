9b9ae9ec6181b6a5e3e262b98210f36bcdbbb62d
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-07-24 21:20:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function firstWord(s) {
   // your code here
+	let s1=""
+	for (let index = 0; index < s.length; index++) {
+		if(s[i]!=" "){
+			s1=s1+s[i]
+		}
+		else{
+			break
+		}
+		
+	}
+	return s1
 }
-
+ 
 // Do not change the code below
 
 const s = prompt("Enter String:");
