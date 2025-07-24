e55d8f1b432c7a037ea5b77cbab7ccc2d7c2458c
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-07-24 21:43:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-function firstWord(s) {
-  // your code here
-	let s1=""
-	for (let i = 0; i < s.length; i++) {
-		if(s[i]!=" "){
-			s1=s1+s[i]
-		}
-		else{
-			s1=s1+'
-			break
-		}
-		
-	}
-	return s1
+function firstWord(str) {
+  // Trim the string and split by one or more whitespace characters
+  const words = str.trim().split(/\s+/);
+  // Return the first word or empty string if none exists
+  return words[0] || '';
 }
- 
-// Do not change the code below
 
+// Do not change the code below
 const s = prompt("Enter String:");
 alert(firstWord(s));
